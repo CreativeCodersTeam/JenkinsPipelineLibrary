824ff2e4a10f8c4f4d5a148eb02198872280c687
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-12-07 22:19:51 +0100

    First attempt for moving xunit test result recording into a new method

---
+++ vars/publishXUnitResults.groovy
@@ -0,0 +1,11 @@
+def call(){
+    step([$class: 'XUnitPublisher', testTimeMargin: '3000', thresholdMode: 2,
+        thresholds: [
+            [$class: 'FailedThreshold', failureNewThreshold: '', failureThreshold: '0', unstableNewThreshold: '', unstableThreshold: ''],
+            [$class: 'SkippedThreshold', failureNewThreshold: '', failureThreshold: '0', unstableNewThreshold: '', unstableThreshold: '']
+        ],
+        tools: [
+            [$class: 'XUnitDotNetTestType', deleteOutputFiles: true, failIfNotNew: true, pattern: 'artifacts/test_results/*.xml', skipNoTestFiles: true, stopProcessingIfError: true]
+        ]
+    ])
+}
