505d1b9546843d9bd2cf7774fc88f76fa1de5e04
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-12-08 20:49:48 +0100

    Dynamically building nuke command

--- vars/nuke.groovy
+++ vars/nuke.groovy
@@ -3,5 +3,11 @@ def call(Map args) {
     def command = 'nuke'
     command = command + " ${args.target}"
 
+    for(def argument in args) {
+        if (argument.key != 'target') {
+            command = command + " --${argument.key} \"${argument.value}\""
+        }        
+    }
+
     sh command
 }
