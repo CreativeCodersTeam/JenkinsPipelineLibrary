644eaa8443923caf046c8ea0e0917572fcd373c6
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-12-11 22:05:29 +0100

    Removed test echo

--- vars/nuke.groovy
+++ vars/nuke.groovy
@@ -3,8 +3,6 @@ def call(Map args) {
     def command = 'nuke'
     command = command + " ${args.target}"
 
-    echo 'API_KEY ' + env.API_KEY
-
     for(def argument in args) {        
         if (argument.key != 'target') {
             def key = argument.key
