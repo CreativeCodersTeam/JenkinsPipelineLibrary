d75d54402c2fd1167b01419b4e87f303c0c01b6b
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-12-15 20:46:28 +0100

    New method for sending status mails

---
+++ vars/sendBuildStatusMail.groovy
@@ -0,0 +1,10 @@
+def call (String statusVerb){
+    def jobName = env.JOB_NAME.replaceAll("%2F", "/")
+    def url = env.BUILD_URL.replaceAll("%252F", "%2F")
+
+    mail to: env.NOTIFY_MAIL_TO,
+         subject: "BUILD ${jobName} ${statusVerb}",
+         charset: 'UTF-8',
+         mimeType: 'text/html',
+         body: "<b>Project: ${jobName}</b> <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${url}"
+}
