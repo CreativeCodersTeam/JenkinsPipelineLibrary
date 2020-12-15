b9cb1ec2d8bd7ffa8b588031ab0234e6657a9348
darthsharp <48331467+darthsharp@users.noreply.github.com>
2020-12-15 20:49:17 +0100

    First set of methods for jenkins build pipeline (#1)

---
+++ vars/nuke.groovy
@@ -0,0 +1,16 @@
+def call(Map args) {
+
+    def command = 'nuke'
+    command = command + " ${args.target}"
+
+    for(def argument in args) {        
+        if (argument.key != 'target') {
+            def key = argument.key
+            def value = argument.value
+
+            command = command + ' --' + key + ' \"' + value + '\"'
+        }        
+    }
+
+    sh command
+}
